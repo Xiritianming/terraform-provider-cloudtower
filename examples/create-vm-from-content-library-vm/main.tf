bcd2d6945b46461b57dad99c8396a326f83785e5
Xiritianming <zhonghuaiyong@gmail.com>
2024-08-29 17:01:38 +0800

    [doc] Update example `create-vm-from-content-library-vm`

--- examples/create-vm-from-content-library-vm/main.tf
+++ examples/create-vm-from-content-library-vm/main.tf
@@ -14,26 +14,26 @@ locals {
 }
 
 provider "cloudtower" {
-  username          = "root"
-  password          = "111111"
-  user_source       = "LOCAL"
-  cloudtower_server = var.config["tower_server"]
+  username          = var.cloudtower_user
+  password          = var.cloudtower_password
+  user_source       = var.cloudtower_user_source
+  cloudtower_server = var.cloudtower_server
 }
 
 data "cloudtower_cluster" "query_clusters" {
-  name_in = var.config["cluster"]
+  name_in = var.clusters
 }
 
 data "cloudtower_host" "query_hosts" {
-  management_ip_in = var.config["host"]
+  management_ip_in = var.hosts
 }
 
-data "cloudtower_content_library_vm_template" "query_templates" {
-  name = var.config["template_name"]
+data "cloudtower_content_library_vm_template" "query_template" {
+  name = var.template_name
 }
 
 data "cloudtower_vlan" "query_vlans" {
-  name_in = var.config["portgroup"]
+  name_in = var.vm_portgroup
 }
 
 locals {
@@ -75,26 +75,29 @@ locals {
 # }
 
 resource "cloudtower_vm" "vms_create_from_template" {
-  count      = length(var.config["vm_ip"])
-  cluster_id = local.cluster_name_map[var.config["cluster"][count.index]].id #data.cloudtower_cluster.query_clusters.clusters[0].id
-  host_id    = local.host_ip_map[var.config["host"][count.index]].id         #data.cloudtower_host.query_hosts.hosts[0].id
-  name       = var.config["vm_name"][count.index]
-  memory     = var.config["memory"][count.index] * local.MB
-  vcpu       = var.config["vcpu"][count.index]
+  count      = length(var.vm_ip)
+  cluster_id = length(var.clusters) > 1 ? local.cluster_name_map[var.clusters[count.index]].id : data.cloudtower_cluster.query_clusters.clusters[0].id
+  # cluster_id = data.cloudtower_cluster.query_clusters.clusters[0].id
+  host_id = var.hosts == null ? null : local.host_ip_map[var.hosts[count.index]].id
+  # host_id = data.cloudtower_host.query_hosts.hosts[0].id
+  name   = var.vm_name[count.index]
+  vcpu   = var.vm_cpu
+  memory = var.vm_memory * local.GB
+  status = var.vm_status
   create_effect {
     is_full_copy                        = false
-    clone_from_content_library_template = data.cloudtower_content_library_vm_template.query_templates.content_library_vm_templates[0].id
+    clone_from_content_library_template = data.cloudtower_content_library_vm_template.query_template.content_library_vm_templates[0].id
     cloud_init {
-      hostname              = var.config["host_name"][count.index]
-      nameservers           = var.config["dns"][count.index]
-      default_user_password = 111111
+      hostname              = var.vm_hostname[count.index]
+      nameservers           = var.vm_dns
+      default_user_password = var.vm_password
       networks {
-        type       = "IPV4"
+        type       = var.vm_network_type
         nic_index  = 0
-        ip_address = var.config["vm_ip"][count.index]
-        netmask    = cidrnetmask("${var.config["vm_ip"][count.index]}/${var.config["cidr"][count.index]}")
+        ip_address = var.vm_ip[count.index]
+        netmask    = cidrnetmask("${var.vm_network_gateway}/${var.vm_network_cidr}")
         routes {
-          gateway = var.config["default_gw"][count.index]
+          gateway = var.vm_network_gateway
           netmask = "0.0.0.0"
           network = "0.0.0.0"
         }
@@ -102,36 +105,37 @@ resource "cloudtower_vm" "vms_create_from_template" {
     }
   }
   dynamic "disk" {
-    for_each = data.cloudtower_content_library_vm_template.query_templates.content_library_vm_templates[0].vm_templates[0].disks
+    # original template's disks
+    for_each = data.cloudtower_content_library_vm_template.query_template.content_library_vm_templates[0].vm_templates[0].disks
     content {
       boot = disk.value.boot
       bus  = disk.value.bus
       vm_volume {
         storage_policy = disk.value.storage_policy
-        name           = "${var.config["vm_name"][count.index]}-${disk.key + 1}"
+        name           = "${var.vm_name[count.index]}-${disk.key + 1}"
         size           = disk.value.size
         origin_path    = disk.value.path
       }
     }
   }
   dynamic "disk" {
-    for_each = var.config["extra_disks"][count.index]
+    # extra disk
+    for_each = var.vm_extra_disks
     content {
-      boot = length(data.cloudtower_content_library_vm_template.query_templates.content_library_vm_templates[0].vm_templates[0].disks) + disk.key
-      bus  = "VIRTIO"
+      boot = length(data.cloudtower_content_library_vm_template.query_template.content_library_vm_templates[0].vm_templates[0].disks) + (disk.key + 1)
+      bus  = disk.value.bus
       vm_volume {
-        storage_policy = "REPLICA_2_THIN_PROVISION"
-        name           = "${var.config["vm_name"][count.index]}-${length(data.cloudtower_content_library_vm_template.query_templates.content_library_vm_templates[0].vm_templates[0].disks) + disk.key + 1}"
-        size           = disk.value * local.GB
+        storage_policy = disk.value.storage_policy
+        name           = "${var.vm_name[count.index]}-${length(data.cloudtower_content_library_vm_template.query_template.content_library_vm_templates[0].vm_templates[0].disks) + (disk.key + 1)}"
+        size           = disk.value.size * local.GB
       }
     }
   }
 
   cd_rom {
-    boot   = length(data.cloudtower_content_library_vm_template.query_templates.content_library_vm_templates[0].vm_templates[0].disks) + length(var.config["extra_disks"][count.index]) + 1
-    iso_id = ""
+    boot = length(data.cloudtower_content_library_vm_template.query_template.content_library_vm_templates[0].vm_templates[0].disks) + length(var.vm_extra_disks[count.index]) + 1
   }
   nic {
-    vlan_id = local.vlan_name_map[var.config["portgroup"][count.index]][local.cluster_name_map[var.config["cluster"][count.index]].id].id #data.cloudtower_vlan.query_vlans.vlans[0].id
+    vlan_id = length(var.vm_portgroup) > 1 ? local.vlan_name_map[var.vm_portgroup[count.index]][local.cluster_name_map[var.clusters[count.index]].id].id : data.cloudtower_vlan.query_vlans.vlans[0].id
   }
 }
--- examples/create-vm-from-content-library-vm/variable.tf
+++ examples/create-vm-from-content-library-vm/variable.tf
@@ -1,22 +1,104 @@
-variable "config" {
-  type = object({
-    tower_server  = string,
-    template_name = string,
-    cluster       = list(string),
-    host          = list(string),
-    portgroup     = list(string),
-    default_gw    = list(string),
-    host_name     = list(string),
-    vm_name       = list(string),
-    memory        = list(number),
-    vcpu          = list(number),
-    cidr          = list(string),
-    vm_ip         = list(string),
-    dns           = list(list(string)),
-    extra_disks   = list(list(number))
-  })
+variable "cloudtower_user" {
+  type = string
+}
+
+variable "cloudtower_password" {
+  type = string
+}
+
+variable "cloudtower_user_source" {
+  type    = string
+  default = "local"
+}
+
+variable "cloudtower_server" {
+  type = string
+}
+
+variable "clusters" {
+  type = list(string)
+}
+
+variable "hosts" {
+  type     = list(string)
+  default  = null
+  nullable = true
+}
+
+
+variable "template_name" {
+  type = string
+}
+
+variable "vm_name" {
+  type = list(string)
+
+}
+
+variable "vm_ip" {
+  type = list(string)
+}
+
+variable "vm_status" {
+  type = string
+}
+
+variable "vm_cpu" {
+  type = number
+}
+
+variable "vm_memory" {
+  type = number
+}
+
+variable "vm_hostname" {
+  type = list(string)
+}
+
+variable "vm_dns" {
+  type = list(string)
+}
+
+variable "vm_password" {
+  type = string
+}
+
+variable "vm_portgroup" {
+  type = list(string)
+}
+
+variable "vm_network_type" {
+  type        = string
+  description = "`vm_networktype` must be `IPV4` or `IPV4_DHCP`."
   validation {
-    condition     = length(var.config.host_name) == length(var.config.vm_name) && length(var.config.host_name) == length(var.config.memory) && length(var.config.host_name) == length(var.config.vcpu) && length(var.config.host_name) == length(var.config.vm_ip) && length(var.config.host_name) == length(var.config.dns) && length(var.config.host_name) == length(var.config.extra_disks)
-    error_message = "Host_name, vm_name, memory, vcpu, vm_ip, dns, extra_disks must have the same length."
+    condition     = contains(["IPV4", "IPV4_DHCP"], var.vm_network_type)
+    error_message = "`vm_networktype` must be `IPV4` or `IPV4_DHCP`."
+  }
+}
+
+variable "vm_network_gateway" {
+  type = string
+}
+
+variable "vm_network_cidr" {
+  type = number
+}
+
+variable "vm_extra_disks" {
+  type = list(
+    object({
+      bus            = string
+      storage_policy = string
+      size           = number
+    })
+  )
+}
+
+variable "validation_vm_counts" {
+  type    = bool
+  default = true
+  validation {
+    condition     = var.hosts == null ? var.validation_vm_counts == true && length(var.vm_name) == length(var.vm_ip) && length(var.vm_name) == length(var.vm_hostname) : var.validation_vm_counts == true && length(var.vm_name) == length(var.vm_ip) && length(var.vm_name) == length(var.vm_hostname) && length(var.vm_name) == length(var.hosts)
+    error_message = "`vm_ip`, `vm_name`, `vm_ip`, `hosts` must have the same length."
   }
 }
