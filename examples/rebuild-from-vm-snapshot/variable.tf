b6fed61abaa49c722c23de6c900522fddc212f7d
Xiritianming <zhonghuaiyong@gmail.com>
2024-09-05 11:56:36 +0800

    [doc] Update example `rebuild-from-vm-snapshot`

---
+++ examples/rebuild-from-vm-snapshot/variable.tf
@@ -0,0 +1,16 @@
+variable "cloudtower_user" {
+  type = string
+}
+
+variable "cloudtower_password" {
+  type = string
+}
+
+variable "cloudtower_user_source" {
+  type    = string
+  default = "local"
+}
+
+variable "cloudtower_server" {
+  type = string
+}
